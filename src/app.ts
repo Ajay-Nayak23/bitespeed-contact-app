46261be20e43573447082fdfd7350814883eb3f2
Ajay-Nayak23 <123745204+Ajay-Nayak23@users.noreply.github.com>
2025-05-28 01:05:03 +0530

    Use process.env.PORT for Render compatibility

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import identifyRoutes from "./routes/identify";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
